9cca812fcfff7b1aedd1c4ef87608f5bec979905
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-22 19:30:23 +0200

    Updated Input atom to more props. Adding also default values.

--- src/components/atoms/Input/Input.jsx
+++ src/components/atoms/Input/Input.jsx
@@ -1,3 +1,21 @@
-export const Input = ({ className, type, onChange }) => {
-  return <input className={className} type={type} onChange={onChange} />;
+export const Input = ({
+  className,
+  type = 'text',
+  name = '',
+  pattern = '',
+  title = '',
+  isRequired = false,
+  onChange = () => {},
+}) => {
+  return (
+    <input
+      className={className}
+      type={type}
+      onChange={onChange}
+      name={name}
+      pattern={pattern}
+      title={title}
+      required={isRequired}
+    />
+  );
 };
