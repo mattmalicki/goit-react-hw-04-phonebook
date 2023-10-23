c97c6aff679883b9b57871743a921812e0c864ef
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-23 15:52:17 +0200

    changed name of some props to proper name

--- src/components/organisms/ContactList/ContactList.jsx
+++ src/components/organisms/ContactList/ContactList.jsx
@@ -4,9 +4,9 @@ import styles from './ContactList.module.css';
 export const ContactList = ({ contacts, onClick }) => {
   return (
     <ul className={styles.ContactList} onClick={onClick}>
-      {contacts.map(contact => {
-        <ContactListItem key={contact.id} item={contact} classes={styles} />;
-      })}
+      {contacts.map(contact => (
+        <ContactListItem key={contact.id} item={contact} classes={styles} />
+      ))}
     </ul>
   );
 };
